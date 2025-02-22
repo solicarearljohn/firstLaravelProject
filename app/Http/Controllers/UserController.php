da0829d7e0f8cd90be564c297004284ad67b8cd9
Earl John Solicar <solicarearljohn@gmail.com>
2025-02-22 20:38:34 +0800

    Add UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['name' => $incomingFields['loginname'], 'password' =>$incomingFields['loginpassword'] ])) {
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+        return back()->withErrors(['login' => 'Invalid login credentials.']);
+    }
+
+    public function logout() {
+        auth()->logout();
+        return redirect('/login');
+    }
+
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'name' => ['required', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'max:10'],
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
+    }
+}
