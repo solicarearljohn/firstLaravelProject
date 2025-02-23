90533858dba5df986e8e59c3f8bd24f19c7dcea2
Earl John Solicar <solicarearljohn@gmail.com>
2025-02-23 08:23:52 +0800

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,10 @@
     <!-- Include Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <!--user icon-->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+
 </head>
 <body>
 
@@ -17,26 +21,15 @@
             <div class="col-12 d-flex justify-content-end mt-3">
                 <form action="/logout" method="POST">
                     @csrf
-                    <button class="btn btn-danger">Log Out</button>
+                    <!-- Check if the user is authenticated -->
+                    @auth
+                    <i class="fas fa-user"></i>&nbsp;{{ auth()->user()->username }}&nbsp;&nbsp;
+                   @endauth
+                    <button class="btn btn-danger">Log Out</button> 
                 </form>
             </div>
         </div>
-
-        @auth
-        <div class="row">
-            <div class="col-12 mt-4">
-                <!-- Welcome Alert -->
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <strong>Hi, {{ auth()->user()->username }}!</strong> Welcome to the homepage, feel free to explore!
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            </div>
-        </div>
-        </div>
-        @endauth
-
-    
-    </div>
+        <br>
 
     
 
