ec13c3aaef2697a43d2d2e1baadd446aadd8433d
Earl John Solicar <solicarearljohn@gmail.com>
2025-02-23 23:50:12 +0800

    updated: all routes protected/only authenticated users can access

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\StudentsController;
 
 
-Route::get('/', function () {
-    return view('home');
-});
+
 
 Route::get('/login', function () {
     return view('login'); // Render the login page (login.blade.php)
@@ -27,10 +25,14 @@ Route::post('/login', [UserController::class, 'login']);   //connected to web.ph
 // Protected Routes (Authenticated Users Only)
 Route::middleware('auth')->group(function () {
 
+    Route::get('/', function () {
+        return view('home');
+    });
     // Students Routes
     Route::get('/students', [StudentsController::class, 'index'])->name('students.index');  // Protected
     Route::get('/students/search', [StudentsController::class, 'search'])->name('students.search');  // Protected
 
+    
     // Student Edit Routes
     Route::get('/students/{id}/edit', [StudentsController::class, 'edit'])->name('students.edit');  // Protected
     Route::put('/students/{id}', [StudentsController::class, 'update'])->name('students.update');  // Protected
@@ -40,12 +42,16 @@ Route::middleware('auth')->group(function () {
 
     // Add new student (protected)
     Route::post('/add-new', [StudentsController::class, 'addNewStudent'])->name('std.addNewStudent');  // Protected
+    // NOte: please take out this from }); to view with out auth Public Students View Route (If needed to be public)
+    Route::get('/', [StudentsController::class, 'myView'])->name('std.myView'); // Public if required
+
 });
 
-// Public Students View Route (If needed to be public)
-Route::get('/', [StudentsController::class, 'myView'])->name('std.myView'); // Public if required
+// if you want to view public
+// Route::get('/', [StudentsController::class, 'myView'])->name('std.myView'); // Public if required
+
 
 // Example: Public view for home page (no authentication needed)
-Route::get('/home', function () {
-    return view('home');
-});
+//Route::get('/home', function () {
+  //  return view('home');
+//});
