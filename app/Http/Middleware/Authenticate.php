1147c21c628d4c924df0b4e38daceb7e63043368
Earl John Solicar <solicarearljohn@gmail.com>
2025-02-23 23:00:56 +0800

    Created Authenticate middleware

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->guest()) {
+            return redirect()->route('login');  // Redirect to login page if not authenticated
+        }
+
+        return $next($request);
+    }
+}
